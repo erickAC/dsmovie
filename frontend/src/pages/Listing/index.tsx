1117c585eea9132db7a0be05da30c907f63d8cce
erickAC <cerickandrade@gmail.com>
2022-03-09 23:11:39 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,22 @@
 import Pagination from "components/Pagination";
+import MovieStars from "components/MovieStars";
+import MovieScore from "components/MovieScore";
+import MovieCard from "components/MovieCard";
 
 function Listing() {
-    return(
-        <Pagination />
-    )
+  return (
+    <>
+      <Pagination />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
